b25786d077de9debcb0a14ff51990b067df18792
MBIAMY <mbiamysteven@gmail.com>
2025-10-03 14:07:16 +0100

    new B

--- src/environment/db.ts
+++ src/environment/db.ts
@@ -3,20 +3,17 @@ import {TextMessageEntity} from "../domain/entity/text-message-entity";
 import {UserEntity} from "../domain/entity/user-entity";
 
 export const dbDatasourceOptions: DataSourceOptions = {
-    // TODO: replace with your database configuration in the fields bellow:
-       //localhost 127.0.0.1
     type: "mysql",
-    host: "mysql://root:oTsDpWBniRbAfomXFoKJxvCxGLPUtqCC@metro.proxy.rlwy.net:26847/railway",
-    port: 3306,
+    host: "metro.proxy.rlwy.net", // ✅ seulement l’hôte
+    port: 26847, // ✅ récupéré de l’URL
     username: "root",
     password: "oTsDpWBniRbAfomXFoKJxvCxGLPUtqCC",
     database: "railway",
 
-    // No need to change this fields bellow
     synchronize: true,
     logging: false,
-    charset : 'utf8mb4',
+    charset: "utf8mb4",
     entities: [TextMessageEntity, UserEntity],
     migrations: [],
     subscribers: [],
-}
+};
